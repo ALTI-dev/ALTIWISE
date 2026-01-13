532f0e32b7888b7c79d963235f3c5b72b5ded172
ALTI <altiwise@yahoo.com>
2026-01-13 19:08:59 +0000

    Add files via upload

---
+++ GAMALTI/mini_gamalti_v 1.py
@@ -0,0 +1,40 @@
+from  time import sleep 
+print("Mini jogo de perguntas, escolha um (❤️ 💭 ⚔️ 🔥) e responda a questão!")
+escolha=str(input("Emoji?: "))
+print("🥰 Aguarde...")
+sleep(2)
+if escolha=="❤️":
+    str(input( "👉 👉 👉 De que forma o amor pode transformar decisões difíceis em escolhas mais leves?\nSeu argumento: "))
+    print("🤞  🤞 um momento...")
+    sleep(2)
+    print("Legal 🫵 😁 reforçando o seu argumento, o amor funciona como um filtro emocional que dá sentido\nàs escolhas, tornando o peso mais suportável.\nMuito obrigado pela resposta!")
+elif escolha=="💭":
+    str(input("👉 👉 👉 O que distingue um sonho possível de uma ilusão?\nSeu argumento: "))
+    print("🤞  🤞 um momento...")
+    sleep(2)
+    print("Muito obrigado 🙏, aumentando seu raciocínio, um sonho possível gera\nação concreta, enquanto a ilusão se dissolve sem criar movimento real.")
+elif escolha=="⚔️":
+    str(input("👉 👉 👉 O que torna uma derrota mais significativa do que uma vitória?\nArgumenta aqui: "))
+    print("🤞  🤞 um momento...")
+    sleep(2)
+    print("😁 Agradeço de fundo pelo argumento e reforçando-o, a derrota expõe fragilidades e\nensina resiliência, enquanto a vitória apenas confirma o que já foi conquistado.")
+elif escolha=="🔥":
+    str(input("👉 👉 👉 Como distinguir paixão genuína de simples entusiasmo passageiro?\nArgumento:"))
+    print("🤞  🤞 um momento...")
+    sleep(2)
+    print("Haaaa muito obg, vou só dar uma mãozinha, a paixão genuína resiste ao tempo\ne às dificuldades, enquanto o entusiasmo desaparece com a primeira barreira.")
+else:
+    print("Parece que o emoji está errado, tem que ser igual a ❤️  🌨️  ⚔️  🔥 tente novamente!")
+    
+print("✨ Fim do mini jogo! Obrigado por refletir comigo ✨")
+
+
+
+
+
+
+
+
+
+
+ 
